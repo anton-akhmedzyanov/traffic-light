a085a87b099226878e3bec11fd22463418f48b2d
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2023-02-07 22:32:00 +0300

    Initial Commit

---
+++ traffic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic light
+//
+//  Created by Anton Akhmedzyanov on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
