5769f3399faac2a747ff38955d1a4f205bed628d
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2023-02-08 22:22:32 +0300

    hghhg

--- traffic light/ViewController.swift
+++ traffic light/ViewController.swift
@@ -1,19 +1,53 @@
-//
-//  ViewController.swift
-//  traffic light
-//
-//  Created by Anton Akhmedzyanov on 07.02.2023.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        offlights()
+       
     }
+    
+    override func viewDidLayoutSubviews() {
+        redView.layer.cornerRadius = view.frame.width / 2
+        yellowView.layer.cornerRadius = view.frame.width / 2
+        greenView.layer.cornerRadius = view.frame.width / 2
+        }
+    
+    private func offlights() {
+        redView.alpha = 0.2
+        yellowView.alpha = 0.2
+        greenView.alpha = 0.2
+    }
+    
+    
+  
+    private var counter = 0
 
-
+    @IBAction func pressedStartButton() {
+        startButton.setTitle(redView.alpha == 0.2 ? "START": "NEXT", for: .normal)
+        counter += 1
+        switch counter {
+        case 1:
+            greenView.alpha = 0.2
+            redView.alpha = 1
+        case 2:
+            redView.alpha = 0.2
+            yellowView.alpha = 1
+        default:
+            yellowView.alpha = 0.2
+            greenView.alpha = 1
+            counter = 0
+        }
+    }
+    
+    
 }
-
